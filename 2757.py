fafedee8e818fd32650a9283812990e73bdc977a
Cleber Schroeder Fonseca <clebersfonseca@users.noreply.github.com>
2021-10-04 23:47:02 -0300

    Update 2757.py
    Correção na exibição da saída da linha 9.

--- 2757.py
+++ 2757.py
@@ -4,5 +4,5 @@ c = int(input())
 
 print('A = %d, B = %d, C = %d' % (a, b, c))
 print('A = %10.0d, B = %10d, C = %10d' % (a, b, c))
-print('A = %s, B = %10d, C = %10d' % (str(a).zfill(10), b, c))
+print('A = %s, B = %s, C = %s' % (str(a).zfill(10), str(b).zfill(10), str(c).zfill(10)))
 print('A = {:<10}, B = {:<10}, C = {:<10}'.format(a, b, c))
